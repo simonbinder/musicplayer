80f9611f1d4b540742823477ff7c30942b0a586d
Jannik Lorenz <jannik.lorenz@moovel.com>
2017-11-29 15:15:02 +0100

    Added register functionality

--- server/routes/router.js
+++ server/routes/router.js
@@ -1,3 +1,4 @@
+const User = require('../models/User');
 
 const headers = {
   'Content-Type': 'application/json',
@@ -10,6 +11,51 @@ module.exports.dispatch = (req, res) => {
   //POST = /register
   if(req.method === 'POST' && req.url === '/register') {
     console.log('Register');
+
+    let body = '';
+    req.on('data', chunk => {
+      body += chunk.toString();
+    });
+
+    req.on('end', () => {
+      const data = JSON.parse(body);
+
+      const {
+        email,
+        password
+      } = data;
+
+      let valid = email !== '' && email != null
+                  password !== '' && password != null;
+
+      if(valid) {
+
+        User.create({
+          email: email,
+          password: password,
+        }, (err, user) => {
+          if(err || user == null) {
+            res.writeHead(400, headers);
+            res.end(JSON.stringify({
+              error: err ? err : 'user == null',
+            }));
+          } else {
+            res.writeHead(200, headers);
+            res.end(JSON.stringify({
+              success: true,
+            }));
+          }
+
+        });
+
+      } else {
+        res.writeHead(400, headers);
+        res.end(JSON.stringify({
+          error: 'Invalid request',
+        }));
+      }
+
+    });
     return;
   }
 
--- server/server.js
+++ server/server.js
@@ -1,6 +1,15 @@
 const http = require('http');
 const port = 8000;
 const router = require('./routes/router');
+const mongoose = require('mongoose');
+
+mongoose.connect('mongodb://localhost:27017/data/db')
+  .then(() => {
+    console.log('Successfully connected to mongodb');
+  })
+  .catch(err => {
+    console.log('Error connecting to mongodb', err);
+  })
 
 const server = http.createServer((request, response) => {
   router.dispatch(request, response);
